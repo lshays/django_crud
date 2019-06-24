3ad92931f25ed3e76cbb1dceaa33d820cc0419b4
lshays <lukeshays@gmail.com>
2019-06-23 21:19:04 -0400

    Added login page

--- crud/views.py
+++ crud/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def index(request):
     return HttpResponse("Hello, world!")
