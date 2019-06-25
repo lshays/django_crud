69426af8947c8113a969805201b66ce3758f4c97
lshays <lukeshays@gmail.com>
2019-06-24 22:48:40 -0400

    Create page is now functional

--- crud/views.py
+++ crud/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from .models import Person
 
 # Create your views here.
 @login_required
@@ -14,4 +15,13 @@ def search(request):
 
 @login_required
 def create(request):
-    return render(request, 'crud/create.html')
+    context = {}
+    if request.POST:
+        name = request.POST["id_name"]
+        date = request.POST["id_date"]
+        number = request.POST["id_number"]
+        p = Person(name=name, date=date, number=number)
+        p.save()
+        context["success"] = True
+        context["Person"] = p
+    return render(request, 'crud/create.html', context)
