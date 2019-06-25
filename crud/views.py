9899400c5c017dbd3fb3548337ec864e51fc8837
lshays <lukeshays@gmail.com>
2019-06-24 20:13:50 -0400

    Added 'person' model

--- crud/views.py
+++ crud/views.py
@@ -7,3 +7,7 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def index(request):
     return render(request, 'crud/index.html')
+
+@login_required
+def search(request):
+    return render(request, 'crud/search.html')
